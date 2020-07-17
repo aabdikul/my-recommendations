ef9eb6963cb5d64399c35501251ebf2c6d32944f
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-07-17 02:14:26 -0400

    Added styling to cards to allow left and right split.

--- my-recommendations-frontend/src/index.js
+++ my-recommendations-frontend/src/index.js
@@ -23,22 +23,29 @@ function renderBooks(books) {
 		card.setAttribute("class", "card")
 		card.setAttribute("id", "container")
 
+		let left = document.createElement('div')
+		left.setAttribute("class", "left")
+		card.appendChild(left)
+
+		let right = document.createElement('div')
+		right.setAttribute("class", "right")
+		card.appendChild(right)
+
 		let h3 = document.createElement('h3')
 		h3.innerHTML = e.title
-		card.appendChild(h3)
+		left.appendChild(h3)
 
 		let h4 = document.createElement('h4')
 		h4.innerHTML = "By: " + e.author
-		card.appendChild(h4)
-
-		let p1 = document.createElement('p')
-		p1.innerHTML = "Genre: " + e.genre
-		card.appendChild(p1)
+		left.appendChild(h4)
 
 		let p2 = document.createElement('p')
 		p2.innerHTML = "Description: " + e.description
-		card.appendChild(p2)
+		left.appendChild(p2)
 
+		let p1 = document.createElement('p')
+		p1.innerHTML = "Genre: " + e.genre
+		left.appendChild(p1)
 
 		main.appendChild(card)
 	})
