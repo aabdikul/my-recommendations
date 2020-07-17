61a42ef39b7817816d523ae977fdde8459601404
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-07-17 01:46:26 -0400

    Added CSS to render cards. Updated fetch command to fetch when DOM loads. Added renderBooks function to render cards.

--- my-recommendations-frontend/src/index.js
+++ my-recommendations-frontend/src/index.js
@@ -2,15 +2,45 @@ const BOOKS_URL = "http://localhost:3000/books"
 
 document.addEventListener("DOMContentLoaded", function() {
 	fetchBooks();
-})
+})	
 
 function fetchBooks() {
+	//Grabbing the full array of all of the books from my backend DB. 
 	fetch(BOOKS_URL)
 		.then(function(response) {
 		return response.json();
 		})
 		.then(function(json) {
-			console.log(json)
+		return renderBooks(json);
 		});
+}
+
+function renderBooks(books) {
+	let main = document.querySelector('main')
+
+	books.map(function(e) {
+		let card = document.createElement('div')
+		card.setAttribute("class", "card")
+		card.setAttribute("id", "container")
+
+		let h3 = document.createElement('h3')
+		h3.innerHTML = e.title
+		card.appendChild(h3)
+
+		let h4 = document.createElement('h4')
+		h4.innerHTML = "By: " + e.author
+		card.appendChild(h4)
+
+		let p1 = document.createElement('p')
+		p1.innerHTML = "Genre: " + e.genre
+		card.appendChild(p1)
+
+		let p2 = document.createElement('p')
+		p2.innerHTML = "Description: " + e.description
+		card.appendChild(p2)
+
+
+		main.appendChild(card)
+	})
 }
 
