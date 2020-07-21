282a46e84bf8034c15f521f720ae8a2707207215
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-07-20 22:04:36 -0400

    Added create and index actions to reviews controller

--- my-recommendations-backend/app/controllers/reviews_controller.rb
+++ my-recommendations-backend/app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+
+	def index
+		reviews = Review.all
+		render json: reviews.to_json(
+			:except => [:created_at, :updated_at]
+		)
+	end	
+
+	def create
+		review = Review.new(params.require(:review).permit(:book_id, :review))
+		review.save
+		render json: review.to_json(
+			:except => [:created_at, :updated_at]
+			)
+	end
+
 end
